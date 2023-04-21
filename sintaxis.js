e2c80d5a9d3186ae748facfad767e6a54a5af84d
leticr86 <129956311+leticr86@users.noreply.github.com>
2023-04-21 11:12:28 +0200

    Create sintaxis.js

---
+++ sintaxis.js
@@ -0,0 +1,12 @@
+const miLista = [
+  "Juan", // Tu nombre (string)
+  27, // Tu edad (number)
+  true, // ¿Eres desarrollador? (boolean)
+  new Date("1995-05-12"), // Tu fecha de nacimiento (Date)
+  { // Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+    titulo: "El nombre del viento",
+    autor: "Patrick Rothfuss",
+    fecha: 2007,
+    url: "https://www.goodreads.com/book/show/186074.The_Name_of_the_Wind"
+  }
+];
